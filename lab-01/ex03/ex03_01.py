79cd10b11f545455139b7463f8563bb637858311
Tan Dung <dungtran1872@gmail.com>
2026-05-11 16:14:38 +0700

    Làm xong Ex03_02

--- lab-01/ex03/ex03_01.py
+++ lab-01/ex03/ex03_01.py
@@ -5,4 +5,8 @@ def tinh_tong_so_chan(lst):
             tong += num
     return tong
 
-input_list = input("Nhập danh sách các số, cách nhau bằng ")
+input_list = input("Nhập danh sách các số, cách nhau bằng dầu phẩy: ")
+numbers = list(map(int, input_list.split(',')))
+
+tong_chan = tinh_tong_so_chan(numbers)
+print("Tổng các số chẵn trong List là:", tong_chan)
